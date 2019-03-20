21904732f7cc770bd8fa857c9dbac040129414ed
Ganzer <Ganzeroar@gmail.com>
2019-03-21 01:05:33 +0300

    proj_euler_1.py

--- proj_euler_1.py
+++ proj_euler_1.py
@@ -1,10 +1,9 @@
-a = []
-
-a.append(1)
-
-a.pop()
-
-for x in range(100000):
-    if x % 2 == 0:
-        a.append(x)
-print(a)
+def problem_1(number):
+    var_for_sum = 0
+    for num in range(number):
+        if num % 3 == 0:
+            var_for_sum += num
+        elif num % 5 == 0:
+            var_for_sum += num
+    return var_for_sum
+print(problem_1(1000))
